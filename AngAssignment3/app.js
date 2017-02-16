931f0a082859f74118e439a65ba57a9861dc4f1b
SampadaLondhe <Sampada@any.com>
2017-02-16 09:15:41 +0530

    Angular JS 3rd Assignment solution

---
+++ AngAssignment3/app.js
@@ -0,0 +1,90 @@
+(function() {
+'use strict';
+
+angular.module('NarrowItDownApp',[])
+.controller('NarrowItDownController',NarrowItDownController)
+.service('MenuSearchService',MenuSearchService)
+.directive('foundItems',FoundItemsDirective);
+
+function FoundItemsDirective(){
+  var ddo = {
+    templateUrl: 'MenuList.html',
+    scope : {
+      items: '<',
+      onRemove: '&'
+    },
+    controller: FoundItemsDirectiveController,
+    controllerAs: 'menu',
+    bindToController: true
+  };
+  return ddo;
+}
+
+function FoundItemsDirectiveController(){
+  var list=this;
+  list.IsEmpty = function () {
+    if(list.items!==undefined){
+    if (list.items.length==0) {
+        return true;
+      }
+    else{
+      console.log(list.items);
+        return false;
+      }
+    }
+  };
+}
+
+NarrowItDownController.$inject=["MenuSearchService"];
+function NarrowItDownController(MenuSearchService){
+  var menu=this;
+
+  menu.doSearch=function(){
+      MenuSearchService.getMatchedMenuItems(menu.searchTerm);
+      menu.items=MenuSearchService.getItems();
+      console.log( menu.items);
+  };
+    menu.removeItem=function(itemIndex){
+    MenuSearchService.removeItem(itemIndex);
+  };
+}
+
+MenuSearchService.$inject = ['$http'];
+function MenuSearchService($http){
+  var service = this;
+  var foundItems=[];
+  service.getMatchedMenuItems = function (searchTerm) {
+    var response = $http({
+      method: "GET",
+      url: "https://davids-restaurant.herokuapp.com/menu_items.json",
+    });
+  foundItems=[];
+
+  var promise=response;
+  promise.then(function (response){
+  if(searchTerm!==undefined) {
+   if(searchTerm.trim()!==""){
+     for(var i=0;i<response.data.menu_items.length;i++){
+       var desc=response.data.menu_items[i].description.toLowerCase();
+       var ser=searchTerm.toLowerCase();
+       if(desc.indexOf(ser)>=0){
+         foundItems.push(response.data.menu_items[i]);
+       }
+     }
+   }
+ }
+})
+.catch(function(error){
+  console.log(error);
+});
+};
+
+service.getItems=function(){
+    return foundItems;
+};
+
+service.removeItem=function(itemIndex){
+    foundItems.splice(itemIndex,1);
+};
+}
+})()
